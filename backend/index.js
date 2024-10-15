425cb34057c750e230779a48eb8214097c1024bb
unlimitedinfinit <abramslive@gmail.com>
2024-10-14 19:52:34 -0600

    added backend and docker containerization
    everything is working for front and backend services, current need to setup firebase tokens to test app.

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from the backend, i too was once a man who went to pages and saw things before i took an arrow to the face!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
